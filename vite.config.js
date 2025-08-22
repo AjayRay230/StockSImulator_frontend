63a82ff8a4f9040c338dc68ae1d5ae5062bdffe0
royajaygit <ajayray2409@gmail.com>
2025-08-23 00:05:57 +0530

    Fix: use Flyway core for PostgreSQL instead of MySQL

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',   // 👈 this fixes asset paths on Netlify
   build: {
-    chunkSizeWarningLimit: 2000, // increase limit to avoid warnings
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // all dependencies from node_modules go into a vendor chunk
-            return 'vendor';
+            return 'vendor'
           }
-          // optional: split large internal modules
           if (id.includes('src/components/HeavyComponent')) {
-            return 'heavy';
+            return 'heavy'
           }
         },
       },
