f7f3af3c6c9f75670f16704624a6fef8a8941fbf
royajaygit <ajayray2409@gmail.com>
2026-02-14 01:36:00 +0530

    search box fixing

--- src/Components/stock/searchStock.jsx
+++ src/Components/stock/searchStock.jsx
@@ -17,7 +17,7 @@ const[showSuggestion,setShowSuggestion] = useState(false);
     e.preventDefault();
     try {
       const token = localStorage.getItem("token");
-      const response = await axios.get(`https://stocksimulator-backend.onrender.com/api/stock/search?query=${symbol}`,{
+      const response = await axios.get(`https://stocksimulator-backend.onrender.com/api/stock/${symbol}`,{
         headers:{
                         Authorization:`Bearer ${token}`
                 }
