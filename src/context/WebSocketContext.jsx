e4d71f3622ad2a15ea2b925cacf7db38ad158aaf
royajaygit <ajayray2409@gmail.com>
2026-02-16 14:12:04 +0530

    updated websocketcontext and added token for validation

--- src/context/WebSocketContext.jsx
+++ src/context/WebSocketContext.jsx
@@ -8,38 +8,55 @@ export const WebSocketProvider = ({ children }) => {
 
   const [latestUpdate, setLatestUpdate] = useState(null);
 
-useEffect(() => {
-  console.log("🚀 WebSocketProvider mounted");
+  useEffect(() => {
 
-  const socket = new SockJS(
-    "https://stocksimulator-backend.onrender.com/ws"
-  );
+    console.log("🚀 WebSocketProvider mounted");
 
-  socket.onopen = () => console.log("🟢 SockJS open");
-  socket.onerror = (e) => console.log("🔴 SockJS error", e);
-  socket.onclose = () => console.log("🟡 SockJS closed");
+    const token = localStorage.getItem("token");
 
-  const client = new Client({
-    webSocketFactory: () => socket,
-    reconnectDelay: 5000,
-
-    onConnect: () => {
-      console.log("✅ STOMP connected");
-
-      client.subscribe("/topic/prices", (message) => {
-        console.log("📈 Message received");
-      });
-    },
-
-    onStompError: (frame) => {
-      console.error("STOMP Error:", frame);
+    if (!token) {
+      console.log("❌ No JWT token found. WebSocket not started.");
+      return;
     }
-  });
 
-  client.activate();
+    const socket = new SockJS(
+      "https://stocksimulator-backend.onrender.com/ws"
+    );
 
-  return () => client.deactivate();
-}, []);
+    socket.onopen = () => console.log("🟢 SockJS open");
+    socket.onerror = (e) => console.log("🔴 SockJS error", e);
+    socket.onclose = () => console.log("🟡 SockJS closed");
+
+    const client = new Client({
+      webSocketFactory: () => socket,
+      reconnectDelay: 5000,
+
+      connectHeaders: {
+        Authorization: `Bearer ${token}`
+      },
+
+      onConnect: () => {
+        console.log("✅ STOMP connected");
+
+        client.subscribe("/topic/prices", (message) => {
+          const data = JSON.parse(message.body);
+          console.log("📈 Message received:", data);
+          setLatestUpdate(data);
+        });
+      },
+
+      onStompError: (frame) => {
+        console.error("❌ STOMP Error:", frame);
+      }
+    });
+
+    client.activate();
+
+    return () => {
+      client.deactivate();
+    };
+
+  }, []);
 
   return (
     <WebSocketContext.Provider value={{ latestUpdate }}>
